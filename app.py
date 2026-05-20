b8b6ab21c2cbdcd9f7e76ee5048bf06edf2ee45e
janhavi-code2748 <janhavichoudhary2748@gmail.com>
2026-05-20 14:06:41 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+expenses = []
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+
+    if request.method == "POST":
+
+        date = request.form["date"]
+        category = request.form["category"]
+        description = request.form["description"]
+        amount = float(request.form["amount"])
+
+        expense = {
+            "date": date,
+            "category": category,
+            "description": description,
+            "amount": amount
+        }
+
+        expenses.append(expense)
+
+    return render_template("index.html", expenses=expenses)
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
